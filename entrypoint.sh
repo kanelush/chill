c3fd8410e4a551154d94286d41e3970b971ea790
kanelush <kanelushi@protonmail.com>
2022-01-24 15:13:25 +0000

    chillincode22124

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+python manage.py compress --no-input
+
+gunicorn dgsap.wsgi:application --bind 0.0.0.0:8000
+
