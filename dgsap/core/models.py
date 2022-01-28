c6890f405e2d904d7a79da3b3340df62c900b63d
kanelush <82923284+kanelush@users.noreply.github.com>
2022-01-28 11:22:46 -0300

    Update models.py

--- dgsap/core/models.py
+++ dgsap/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 import random
 # Create your models here.
 
@@ -16,4 +16,18 @@ class Product(models.Model):
 
    def __str__(self):
         return self.name
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateTimeField()
+    created = models.DateTimeField(default=datetime.datetime.now, blank=True,null=True)
+    fieldo = models.CharField(max_length=20, blank=True)
+    mail = models.EmailField(max_length=254, blank=True)
+   
+
+    def __str__(self):
+        return self.subject
+
 
